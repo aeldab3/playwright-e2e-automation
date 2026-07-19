25f2cdf2da1292ca94ad59d126282e9c3abc2a4c
aeldab3 <aeldabaa2@gmail.com>
2026-07-19 21:33:22 +0300

    refactor: use Playwright recommended locators in login tests

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,16 +2,20 @@ import { test, expect } from '@playwright/test';
 
 test('login', async({page}) => {
     await page.goto('https://practice.expandtesting.com/login');
-    await page.locator("//input[@id='username']").fill("practice");
-    await page.locator("//input[@id='password']").fill("SuperSecretPassword!");
-    await page.locator("//button[@id='submit-login']").click();
-    await expect(page.locator("div[id='flash'] b")).toContainText("You logged into a secure area!");
+    await page.getByLabel("Username").fill("practice");
+    await page.getByLabel("Password").fill("SuperSecretPassword!");
+    await page.getByRole('button', { name : 'Login'}).click();
+    await expect(
+        page.getByText("You logged into a secure area!", { exact: true })
+    ).toBeVisible();
 })
 
 test('invalid login', async({page}) => {
     await page.goto('https://practice.expandtesting.com/login');
-    await page.locator("//input[@id='username']").fill("eldab3");
-    await page.locator("//input[@id='password']").fill("SuperSecretPassword!");
-    await page.locator("//button[@id='submit-login']").click();
-    await expect(page.locator("div[id='flash'] b")).toContainText("Your username is invalid!");
+    await page.getByLabel("Username").fill("eldab3");
+    await page.getByLabel("Password").fill("SuperSecretPassword!");
+    await page.getByRole('button', { name : 'Login'}).click();
+    await expect(
+        page.getByText("Your username is invalid!", { exact: true })
+    ).toBeVisible();
 })
